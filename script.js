6301a56994c0c6b322a10f79e8b563e2f7fdec2b
website7777 <95430611+website7777@users.noreply.github.com>
2023-11-03 19:44:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,198 @@
+function one() {
+    window.location.href = '1.html';
+}
+function two() {
+    window.location.href = '2.html';
+}
+function three() {
+    // Add your code for the third function
+    window.location.href = '3.html';
+}
+function four() {
+    // Add your code for the fourth function
+    window.location.href = '4.html';
+}
+function five() {
+    // Add your code for the fifth function
+    window.location.href = '5.html';
+}
+function six() {
+    // Add your code for the sixth function
+    window.location.href = '6.html';
+}
+function seven() {
+    // Add your code for the seventh function
+    window.location.href = '7.html';
+}
+function eight() {
+    // Add your code for the eighth function
+    window.location.href = '8.html';
+}
+function nine() {
+    // Add your code for the ninth function
+    window.location.href = '9.html';
+}
+function ten() {
+    // Add your code for the tenth function
+    window.location.href = '10.html';
+}
+function eleven() {
+    // Add your code for the eleventh function
+    window.location.href = '11.html';
+}
+function twelve() {
+    // Add your code for the twelfth function
+    window.location.href = '12.html';
+}
+function thirteen() {
+    // Add your code for the thirteenth function
+    window.location.href = '13.html';
+}
+function fourteen() {
+    // Add your code for the fourteenth function
+    window.location.href = '14.html';
+}
+function fifteen() {
+    // Add your code for the fifteenth function
+    window.location.href = '15.html';
+}
+function sixteen() {
+    // Add your code for the sixteenth function
+    window.location.href = '16.html';
+}
+function seventeen() {
+    // Add your code for the seventeenth function
+    window.location.href = '17.html';
+}
+function eighteen() {
+    // Add your code for the eighteenth function
+    window.location.href = '18.html';
+}
+function nineteen() {
+    // Add your code for the nineteenth function
+    window.location.href = '19.html';
+}
+function twenty() {
+    // Add your code for the twentieth function
+    window.location.href = '20.html';
+}
+function twentyOne() {
+    // Add your code for the twenty-first function
+    window.location.href = '21.html';
+}
+function twentyTwo() {
+    // Add your code for the twenty-second function
+    window.location.href = '22.html';
+}
+function twentyThree() {
+    // Add your code for the twenty-third function
+    window.location.href = '23.html';
+}
+function twentyFour() {
+    // Add your code for the twenty-fourth function
+    window.location.href = '24.html';
+}
+function twentyFive() {
+    // Add your code for the twenty-fifth function
+    window.location.href = '25.html';
+}
+function twentySix() {
+    // Add your code for the twenty-sixth function
+    window.location.href = '26.html';
+}
+function twentySeven() {
+    // Add your code for the twenty-seventh function
+    window.location.href = '27.html';
+}
+function twentyEight() {
+    // Add your code for the twenty-eighth function
+    window.location.href = '28.html';
+}
+function twentyNine() {
+    // Add your code for the twenty-ninth function
+    window.location.href = '29.html';
+}
+function thirty() {
+    // Add your code for the thirtieth function
+    window.location.href = '30.html';
+}
+function thirtyOne() {
+    // Add your code for the thirty-first function
+    window.location.href = '31.html';
+}
+function thirtyTwo() {
+    // Add your code for the thirty-second function
+    window.location.href = '32.html';
+}
+function thirtyThree() {
+    // Add your code for the thirty-third function
+    window.location.href = '33.html';
+}
+function thirtyFour() {
+    // Add your code for the thirty-fourth function
+    window.location.href = '34.html';
+}
+function thirtyFive() {
+    // Add your code for the thirty-fifth function
+    window.location.href = '35.html';
+}
+function thirtySix() {
+    // Add your code for the thirty-sixth function
+    window.location.href = '36.html';
+}
+function thirtySeven() {
+    // Add your code for the thirty-seventh function
+    window.location.href = '37.html';
+}
+function thirtyEight() {
+    // Add your code for the thirty-eighth function
+    window.location.href = '38.html';
+}
+function thirtyNine() {
+    // Add your code for the thirty-ninth function
+    window.location.href = '39.html';
+}
+function forty() {
+    // Add your code for the fortieth function
+    window.location.href = '40.html';
+}
+function fortyOne() {
+    // Add your code for the forty-first function
+    window.location.href = '41.html';
+}
+function fortyTwo() {
+    // Add your code for the forty-second function
+    window.location.href = '42.html';
+}
+function fortyThree() {
+    // Add your code for the forty-third function
+    window.location.href = '43.html';
+}
+function fortyFour() {
+    // Add your code for the forty-fourth function
+    window.location.href = '44.html';
+}
+function fortyFive() {
+    // Add your code for the forty-fifth function
+    window.location.href = '45.html';
+}
+function fortySix() {
+    // Add your code for the forty-sixth function
+    window.location.href = '46.html';
+}
+function fortySeven() {
+    // Add your code for the forty-seventh function
+    window.location.href = '47.html';
+}
+function fortyEight() {
+    // Add your code for the forty-eighth function
+    window.location.href = '48.html';
+}
+function fortyNine() {
+    // Add your code for the forty-ninth function
+    window.location.href = '49.html';
+}
+function fifty() {
+    // Add your code for the forty-ninth function
+    window.location.href = '50.html';
+}
